0f6d7828468d5da9a0a60f041955a0a7d4163fb7
Anurag <bhootnath@gmail.com>
2023-03-16 14:26:15 +0530

    final commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express, { response } from 'express';
 import nodemailer from 'nodemailer'
 import cors from 'cors'
+import bodyParser from 'body-parser';
+import multer from 'multer'
+import * as fs from 'fs'
 
 const app = express();
 app.use(cors())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 const AUTH_PATH = '/api/email/';
@@ -11,18 +15,19 @@ const PORT = process.env.PORT || 5000
 
 app.post(`${AUTH_PATH}contactus`, (req, res) => {
     const{ name, email, phone, message } = req.body;
-    console.log(email)
+    console.log(req.body)
 
-      const mailoptions = {
+    const mailoptions = {
     from: email,
     to: 'pratikrajgupta23@gmail.com',
     subject: 'New contact submission!',
     html:  `
-            <h1> YOUR MESSAGE IS:</h1>
-            <p>${message}</p>
-            <p>Email received from : ${email} </p>
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Phone: ${phone}</p>
+            <p>Message: ${message}</p>
         `,
-  };
+    };
 
   const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -41,7 +46,64 @@ try {
 }
 
 });
-    
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/")
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+  },
+})
+const upload = multer({storage: storage})
+
+
+app.post(`${AUTH_PATH}careersapply`, upload.single('resume'), (req, res) => {
+    const{ name, email, phone, jobRole } = req.body;
+    const resumeData = {
+                data: fs.readFileSync('uploads/' + req.file.filename),
+                contentType: req.file.mimetype
+            }
+    const mailoptions = {
+    from: email,
+    to: 'pratikrajgupta23@gmail.com',
+    subject: 'New career submission!',
+    attachments: [
+    {
+      filename: req.file.filename,
+      path: `data:${resumeData.contentType};base64,${resumeData.data.toString('base64')}`
+    }  
+  ],
+    html:  `
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Phone: ${phone}</p>
+            <p>Job Role: ${jobRole}</p>
+
+        `,
+    };
+
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'anuragsinhadeveloper@gmail.com',
+      pass: 'jfsticcyypysyzte',
+    },
+  });
+
+
+try {
+      transporter.sendMail(mailoptions);
+      res.status(200).json({"success" : true})
+} catch (error) {
+    res.status(400).json({error})
+}
+
+  fs.unlink(`uploads/${req.file.filename}`, (err)=>{
+        if(err) console.log(err);
+    })
+});
 
 app.listen(PORT, () => {
     console.log("SERVER IS RUNNING OK")
